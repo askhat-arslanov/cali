7d6e7e4bce6277488e06c4a56bf04c19fb000832
Askhat Arslanov <caeleste961@gmail.com>
2020-08-20 22:53:28 +0300

    Small style fixes

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { Route, Switch } from 'react-router-dom'
 
 import './app.scss'
+import GearIcon from './img/gear.svg'
 import ErrorBoundary from '../error-boundary'
 import Navbar from '../navbar'
 import StengthRoutine from '../strength-routine'
@@ -15,19 +16,21 @@ import WarmUpRoutine from '../warm-up-routine'
 const App = () => {
   return (
     <div className="app">
+      <img className="app__decorate-icon app__decorate-icon--left" src={GearIcon} alt="Gear" />
+      <img className="app__decorate-icon app__decorate-icon--right" src={GearIcon} alt="Gear" />
+
       <Navbar />
+
       <ErrorBoundary>
-        <div className="app__grid">
-          <div className="app__content">
-            <Switch>
-              <Route exact path="/" component={StartPage} />
-              <Route path="/strength" component={StengthRoutine} />
-              <Route path="/skill" component={SkillRoutine} />
-              <Route path="/warm-up" component={WarmUpRoutine} />
-              <Route path="/signin" component={SignIn} />
-              <Route path="/signup" component={SignUp} />
-            </Switch>
-          </div>
+        <div className="app__content">
+          <Switch>
+            <Route exact path="/" component={StartPage} />
+            <Route path="/strength" component={StengthRoutine} />
+            <Route path="/skill" component={SkillRoutine} />
+            <Route path="/warm-up" component={WarmUpRoutine} />
+            <Route path="/signin" component={SignIn} />
+            <Route path="/signup" component={SignUp} />
+          </Switch>
         </div>
       </ErrorBoundary>
     </div>
